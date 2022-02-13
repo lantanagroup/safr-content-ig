6b3d125e89b764adc9da922c953640f60f758dc4
mduna <mdunaji@gmail.com>
2022-02-13 09:39:36 -0500

    test

--- _refresh.bat
+++ _refresh.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
-SET tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
+
+REM SET tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
+SET tooling_jar=tooling-1.4.0-jar-with-dependencies.jar
 SET input_cache_path=%~dp0input-cache
 SET resources_path=%~dp0/input/resources
 SET ig_ini_path=%~dp0ig.ini
