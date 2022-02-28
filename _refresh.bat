f75ec4344979d4a0c07aa17dab95af3ec462f961
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2022-02-28 07:13:17 -0800

    Updating Library resources to remove extra "library-" from ID Updating Measure resource to reference "Initial Population" rather than "Initial Inpatient Population", which doesn't exist in the CQL Updating `_refresh.bat` to upload the measure bundle to cqf-ruler.nhsnlink.org

--- _refresh.bat
+++ _refresh.bat
@@ -13,7 +13,7 @@ GOTO igpublish
 
 :isonline
 ECHO We're online, setting publish to local sandbox FHIR server
-SET fsoption=-fs http://localhost:8080/cqf-ruler-r4/fhir/
+SET fsoption=-fs https://cqf-ruler.nhsnlink.org/fhir
 
 :igpublish
 
