568ed5d33a8447119b6349f347ff4a5c421c5429
ajagann1 <arvind.jagannathan@lantanagroup.com>
2022-11-21 16:03:23 -0500

    Update to refresh.bat to properly pull in all dependencies into the resulting measure bundle.  Updates to the library files to properly match the filepath refresh.bat is expecting

--- _refresh.bat
+++ _refresh.bat
@@ -21,10 +21,10 @@ SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 :: -d %fsoption%
 IF EXIST "%input_cache_path%\%tooling_jar%" (
 	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t -d %fsoption%
-	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t 
+	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t -d
 ) ELSE If exist "..\%tooling_jar%" (
 	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t 
-	JAVA -jar "..\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t
+	JAVA -jar "..\%tooling_jar%" -RefreshIG -ini="%ig_ini_path%" -t -d
 ) ELSE (
 	ECHO IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 )
