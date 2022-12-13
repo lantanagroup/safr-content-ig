ee31bd15db47b790a6ca8368f7bf6027b339768d
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2022-12-12 22:26:13 -0800

    Doubling java heap space

--- _genonce.sh
+++ _genonce.sh
@@ -16,12 +16,12 @@ echo "$txoption"
 
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
-	java -Xmx2046m -Xms2046m -jar $publisher -ig . $txoption $*
+	java -Xmx4092m -Xms4092m -jar $publisher -ig . $txoption $*
 
 else
 	publisher=../$publisher_jar
 	if test -f "$publisher"; then
-		java -Xmx2046m -Xms2046m -jar $publisher -ig . $txoption $*
+		java -Xmx4092m -Xms4092m -jar $publisher -ig . $txoption $*
 	else
 		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
 	fi
