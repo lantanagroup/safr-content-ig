225c6add232ad4d1eea49a698a6510812ba5b085
ajagann1 <arvind.jagannathan@lantanagroup.com>
2023-02-06 10:33:32 -0500

    Removing CDI and HOB individual measures.  Some updates to dQMAcute (CDI/HOB combined)

--- _genonce.sh
+++ _genonce.sh
@@ -14,14 +14,16 @@ fi
 
 echo "$txoption"
 
+export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dfile.encoding=UTF-8"
+
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
-	java -Xmx4092m -Xms4092m -jar $publisher -ig . $txoption $*
+	java -jar $publisher -ig . $txoption $*
 
 else
 	publisher=../$publisher_jar
 	if test -f "$publisher"; then
-		java -Xmx4092m -Xms4092m -jar $publisher -ig . $txoption $*
+		java -jar $publisher -ig . $txoption $*
 	else
 		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
 	fi
