51dad4c2e5c69d5082ada8303007f0d3e292a337
IT <it@lantanagroup.com>
2023-12-08 16:39:54 -0500

    Updating bash scripts to work

--- _refresh.sh
+++ _refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #DO NOT EDIT WITH WINDOWS
-tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
+tooling_jar=tooling-cli-2.5.0-SNAPSHOT.jar
 input_cache_path=./input-cache
 resources_path=$PWD/input/resources
 ig_resource_path=./input/anc-cds.xml
@@ -22,12 +22,12 @@ echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	JAVA -jar $tooling -RefreshIG -root-dir="$PWD" -ip="$ig_resource_path" -t -d -p $fsoption
+	java -jar $tooling -RefreshIG -root-dir="$PWD" -ip="$ig_resource_path" -t -d -p $fsoption
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		JAVA -jar $tooling -RefreshIG -root-dir="$PWD" -ip="$ig_resource_path" -t -d -p $fsoption
+		java -jar $tooling -RefreshIG -root-dir="$PWD" -ip="$ig_resource_path" -t -d -p $fsoption
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 	fi
