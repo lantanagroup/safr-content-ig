58a518c3022859ed3a6ce72c23b2ea415a6f94fd
Michael Misiaszek <michael.misiaszek@lantanagroup.com>
2024-07-24 12:13:40 -0700

    Update _updateCQFTooling.sh
    update for version 3.1.0, also added variable line 31

--- _updateCQFTooling.sh
+++ _updateCQFTooling.sh
@@ -21,14 +21,14 @@ done
 r=snapshots
 g=org.opencds.cqf
 a=tooling-cli
-v=3.0.0-SNAPSHOT
+v=3.1.0-SNAPSHOT
 
 dlurl='https://oss.sonatype.org/service/local/artifact/maven/redirect?r='${r}'&g='${g}'&a='${a}'&v='${v}''
 
 echo ${dlurl}
 
 input_cache_path=./input-cache/
-tooling_jar=tooling-cli-3.0.0-SNAPSHOT.jar
+tooling_jar=tooling_jar=${a}-${v}.jar
 
 set -e
 if ! type "curl" > /dev/null; then
