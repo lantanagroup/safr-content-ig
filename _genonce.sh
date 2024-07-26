fd8adea38cf5e72f9f86b2cd591e5ba2e2119bbb
Michael Misiaszek <michael.misiaszek@lantanagroup.com>
2024-07-26 11:42:52 -0700

    Update _genonce.sh
    Fix Java OPT

--- _genonce.sh
+++ _genonce.sh
@@ -18,7 +18,7 @@ export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dfile.encoding=UTF-8"
 
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
-	java -Xmx4192m-jar $publisher -ig . $txoption $*
+	java -Xmx4192m -jar $publisher -ig . $txoption $*
 
 else
 	publisher=../$publisher_jar
