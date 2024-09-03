eead605bfcb22b402d673e7d21e8981f88788b0e
Michael Misiaszek <michael.misiaszek@lantanagroup.com>
2024-09-03 12:42:47 -0700

    Update _refresh.sh
    Update CQF Tooling to 3.6.0

--- _refresh.sh
+++ _refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #DO NOT EDIT WITH WINDOWS
-tooling_jar=tooling-cli-3.1.0-SNAPSHOT.jar
+tooling_jar=tooling-cli-3.6.0.jar
 input_cache_path=./input-cache
 resources_path=$PWD/input/resources
 ig_ini_path=ig.ini
