8e5d31f6e79ca370160ae2426e0083584d54cc0e
caspears <sagacity@gmail.com>
2026-03-27 06:42:12 -0700

    TECH_DEBT: Preparations for publication

---
+++ _refresh.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#DO NOT EDIT WITH WINDOWS
+tooling_jar=tooling-cli-3.10.0.jar
+input_cache_path=./input-cache
+ig_ini_path=$PWD/ig.ini
+
+#set -e
+#echo Checking internet connection...
+#wget -q --spider tx.fhir.org
+
+if [ $? -eq 0 ]; then
+	echo "Online"
+	fsoption=""
+else
+	echo "Offline"
+	fsoption=""
+fi
+
+echo "$fsoption"
+
+tooling=$input_cache_path/$tooling_jar
+if test -f "$tooling"; then
+	java -jar $tooling -RefreshIG -ini="$ig_ini_path" -d -t $fsoption -x
+else
+	tooling=../$tooling_jar
+	echo $tooling
+	if test -f "$tooling"; then
+		java -jar $tooling -RefreshIG -ini="$ig_ini_path" -d -t $fsoption -x
+	else
+		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
+	fi
+fi
+
+find bundles/ -type f -name "*-bundle.json" -exec sed -i -e "s/-bundle//g" {} \;
